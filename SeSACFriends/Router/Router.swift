853684dabb0e726be18261fa7dff8f504df0a43d
urijan44 <teamsva360@gmail.com>
2022-01-22 21:34:34 +0900

    Protocol Router작성

---
+++ SeSACFriends/Router/Router.swift
@@ -0,0 +1,20 @@
+//
+//  Router.swift
+//  SeSACFriends
+//
+//  Created by hoseung Lee on 2022/01/22.
+//
+
+import UIKit
+
+protocol Router: AnyObject {
+  func present(_ viewController: UIViewController, animated: Bool)
+  func present(_ viewController: UIViewController, animated: Bool, onDismissed: (()->Void)?)
+  func dismiss(anmiated: Bool)
+}
+
+extension Router {
+  func present(_ viewController: UIViewController, animated: Bool) {
+    present(viewController, animated: animated, onDismissed: nil)
+  }
+}
