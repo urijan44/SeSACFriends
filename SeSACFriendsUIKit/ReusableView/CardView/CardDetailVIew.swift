da123b733d1294a0a36b2666d4e64e689c1f975d
urijan44 <teamsva360@gmail.com>
2022-02-11 20:53:58 +0900

    CardDetailView UI

--- SeSACFriendsUIKit/ReusableView/CardView/CardDetailVIew.swift
+++ SeSACFriendsUIKit/ReusableView/CardView/CardDetailVIew.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 internal struct CardDetailView: View {
-  @State var isDetailOpen: Bool = true
-  @Binding var titles: [SeSACTitle]
-
+  @State var isDetailOpen: Bool = false
+  @Binding var titles: [ConvertedTitle]
+  var hobbies: [String]
+  var reviews: [String]
   private var arrowDegree: Double {
     isDetailOpen ? 270 : 90
   }
@@ -18,7 +19,7 @@ internal struct CardDetailView: View {
   public var isSearchView: Bool
 
   var body: some View {
-    VStack {
+    VStack(spacing: 0) {
       HStack{
         Text("김새싹")
           .padding(16)
@@ -38,14 +39,24 @@ internal struct CardDetailView: View {
         SeSACTitleView(titles: $titles)
           .padding(.horizontal, 16)
         if isSearchView {
-          WantHobbyView()
+          WantHobbyView(hobbies: hobbies, viewWidth: UIScreen.main.bounds.width - 32)
+            .padding(EdgeInsets(top: 24, leading: 0, bottom: 0, trailing: 0))
         }
+        ReviewView(review: reviews.first ?? "")
+        Spacer()
       }
     }
     .background(
       RoundedRectangle(cornerRadius: 8)
         .strokeBorder(Color(.seSACGray1), lineWidth: 1)
     )
+  }
+
+  func ShowDetailView() -> some View {
+    VStack {
+
+    }
+    .frame(maxHeight: isDetailOpen ? .infinity : .zero)
   }
 }
 
@@ -55,16 +66,10 @@ struct CardDetailView_Previews: PreviewProvider {
       backgroundImage: Image(uiImage: AssetImage.sesacBackground1.image),
       faceImage: Image(uiImage: AssetImage.sesacFace1.image),
       name: "김새싹",
-      title: [
-        .init(title: "좋은 매너"),
-        .init(title: "정확한 시간 약속"),
-        .init(title: "빠른 응답"),
-        .init(title: "친절한 성격"),
-        .init(title: "능숙한 취미 실력"),
-        .init(title: "유익한 시간"),
-       ],
-      hobby: ["달리기", "뜨개질", "산책"],
-      review: []
+      title: .constant([]),
+      hobbies: ["달리기", "뜨개질", "산책"],
+      reviews: [],
+      isSearchView: true
     )
   }
 }
