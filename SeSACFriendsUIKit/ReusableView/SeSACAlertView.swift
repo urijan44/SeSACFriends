85891d2ef3c5df0e40d76880024a3cacb14ca445
urijan44 <teamsva360@gmail.com>
2022-02-25 00:52:46 +0900

    Mod AlertView: View -> ALertView: ViewModifier

--- SeSACFriendsUIKit/ReusableView/SeSACAlertView.swift
+++ SeSACFriendsUIKit/ReusableView/SeSACAlertView.swift
@@ -7,21 +7,67 @@
 
 import SwiftUI
 
-struct SeSACAlertView: View {
+public struct SeSACAlertView<AlertContent: View>: ViewModifier {
 
+  @Binding var isPresenting: Bool
+  let alertContent: AlertContent
+
+  public func body(content: Content) -> some View {
+    ZStack {
+      content
+      if isPresenting {
+        Color(.seSACBlack).opacity(0.5)
+        alertContent
+      }
+    }
+    .ignoresSafeArea()
+  }
+  public init(isPresenting: Binding<Bool>, @ViewBuilder alertContent: () -> AlertContent) {
+    self._isPresenting = isPresenting
+    self.alertContent = alertContent()
+  }
+
+}
+
+public extension View {
+  func sesacAlert<AlertContent: View>(
+    isPresenting: Binding<Bool>,
+    @ViewBuilder alertContent: @escaping () -> AlertContent) -> some View {
+      self.modifier(SeSACAlertView(isPresenting: isPresenting, alertContent: alertContent))
+    }
+}
+
+//struct SeSACAlertView_Previews: PreviewProvider {
+//  static var previews: some View {
+//    SeSACAlertView(title: "약속을 취소하겠습니까?", message: "약속을 취소하시면 패널티가 부과됩니다.", cancelText: "취소", confirmText: "확인", cancelAction: nil, confirmAction: nil)
+//  }
+//}
+
+public struct SeSACAlert: View {
   let title: String
   let message: String
   let cancelText: String
   let confirmText: String
   var cancelAction: (() -> Void)?
   var confirmAction: (() -> Void)?
-  var body: some View {
-
-    ZStack {
-      Color(.seSACBlack).opacity(0.5)
-      RoundedRectangle(cornerRadius: 16)
-        .foregroundColor(Color(.seSACWhite))
-        .frame(height: 156, alignment: .center)
+  public init(
+    title: String,
+    message: String,
+    cancelText: String = "취소",
+    confirmText: String = "확인",
+    cancelAction: (() -> Void)?,
+    confirmAction: (() -> Void)?) {
+      self.title = title
+      self.message = message
+      self.cancelText = cancelText
+      self.confirmText = confirmText
+      self.cancelAction = cancelAction
+      self.confirmAction = confirmAction
+    }
+  public var body: some View {
+    RoundedRectangle(cornerRadius: 16)
+      .foregroundColor(Color(.seSACWhite))
+      .frame(height: 156, alignment: .center)
       .padding(.horizontal, 16)
       .overlay(
         VStack(spacing: 0) {
@@ -47,12 +93,5 @@ struct SeSACAlertView: View {
           }
         }
       )
-    }
-  }
-}
-
-struct SeSACAlertView_Previews: PreviewProvider {
-  static var previews: some View {
-    SeSACAlertView(title: "약속을 취소하겠습니까?", message: "약속을 취소하시면 패널티가 부과됩니다.", cancelText: "취소", confirmText: "확인", cancelAction: nil, confirmAction: nil)
   }
 }
